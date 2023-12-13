6b5974932b15bc8a42e9990c15fdf89285214b93
Ainis Vabolis <Ainis.Vabolis@adform.com>
2023-12-13 12:59:36 +0200

    count letters and numbers

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿const string filePath = "input.txt";
+
+try
+{
+    if (File.Exists(filePath))
+    {
+        var content = File.ReadAllText(filePath);
+
+        int letterCount = content.Count(char.IsLetter);
+        int digitCount = content.Count(char.IsDigit);   
+
+        Console.WriteLine($"The file contains {letterCount} letters and {digitCount} digits.");
+    }
+    else
+    {
+        Console.WriteLine($"File not found at path: {filePath}");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"An error occurred: {ex.Message}");
+}
+
+Console.WriteLine("\nPress any key to exit...");
+Console.ReadKey();
